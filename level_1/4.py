94ecdcc1dfa90a92acfbdc623c7fcc9cc0391ed8
RusssianNightmare <123409770+RusssianNightmare@users.noreply.github.com>
2026-03-30 17:27:14 +0300

    Update 4.py

--- level_1/4.py
+++ level_1/4.py
@@ -3,8 +3,10 @@ import datetime
 from constants import ___
 
 
-def calculate_age(date_of_birth: ___) -> ___:
-    pass
+def calculate_age(date_of_birth: datetime.date) -> int:
+    today = datetime.date(2022, 6, 2)
+    date_of_birth = datetime.date(1965, 6, 2)
+    return today.year - date_of_birth.year
 
 
 if __name__ == "__main__":
