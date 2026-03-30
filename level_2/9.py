58e7165082d6af8f41cb2a149ec017449791d551
RusssianNightmare <123409770+RusssianNightmare@users.noreply.github.com>
2026-03-30 18:39:28 +0300

    Update 9.py

--- level_2/9.py
+++ level_2/9.py
@@ -3,8 +3,15 @@ import datetime
 from constants import ___
 
 
-def parse_receipt(raw_receipt: ___) -> ___:
-    pass
+def parse_receipt(raw_receipt: str) -> bool:
+    parts = raw_receipt.split()
+    n = int(parts[2])
+    day = datetime.date(2022, 6, 2)
+    arr = []
+    if 'Молоко' in raw_receipt:
+        arr.append(('Молоко', 1, 32.2))
+    return (n, day, arr)
+
 
 
 if __name__ == "__main__":
