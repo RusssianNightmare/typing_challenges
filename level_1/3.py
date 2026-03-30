8faffe40f17c49dc159ca1d35b51a54efbbe1ef6
RusssianNightmare <123409770+RusssianNightmare@users.noreply.github.com>
2026-03-30 17:14:12 +0300

    Update 3.py

--- level_1/3.py
+++ level_1/3.py
@@ -1,8 +1,11 @@
 from constants import ___
 
 
-def compose_full_name(first_name: ___, last_name: ___, middle_name: ___) -> ___:
-    pass
+def compose_full_name(first_name: str, last_name: str, middle_name: str) -> str:
+    if middle_name:
+        return f'{last_name} {first_name} {middle_name}'
+    else:
+        return f'{last_name} {first_name}'
 
 
 if __name__ == "__main__":
