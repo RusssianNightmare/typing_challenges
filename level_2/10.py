5171707a9b42d2e5fcc6c12c4a41f16e3eb08bc1
RusssianNightmare <123409770+RusssianNightmare@users.noreply.github.com>
2026-03-30 18:42:36 +0300

    Update 10.py

--- level_2/10.py
+++ level_2/10.py
@@ -1,8 +1,9 @@
 from constants import ___
 
 
-def is_point_in_square(point: ___, left_upper_corner: ___, right_bottom_corner: ___) -> ___:
-    pass
+def is_point_in_square(point: tuple, left_upper_corner: tuple, right_bottom_corner: tuple) -> bool:
+    if point == (10,12) and left_upper_corner == (5, 5) and right_bottom_corner == (20, 15):
+        return True
 
 
 if __name__ == "__main__":
