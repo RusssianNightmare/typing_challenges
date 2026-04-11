68fae90a3218723cf253bbf703e43c4bb94133b1
RusssianNightmare <123409770+RusssianNightmare@users.noreply.github.com>
2026-04-11 15:42:06 +0300

    Update 6.py

--- level_2/6.py
+++ level_2/6.py
@@ -1,11 +1,12 @@
 from constants import ___
 
 
-def is_name_male(name: str, name_gender_map: dict) -> bool | None:
+def is_name_male(name: str, name_gender_map: dict[str. bool]) -> bool | None:
     if name == 'John':
         return name_gender_map.get(name)
     if name == 'Unknown':
         return name_gender_map.get(name)
+    return None
 
 
 if __name__ == "__main__":
