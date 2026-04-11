17d99a33871d5fcce44083a029f753eddc870ef2
RusssianNightmare <123409770+RusssianNightmare@users.noreply.github.com>
2026-04-11 15:20:21 +0300

    Update 10.py

--- level_1/10.py
+++ level_1/10.py
@@ -1,16 +1,14 @@
 from constants import ___
 
-
-def stringify(value: str | int | None | float) -> bool:
+def stringify(value: str | int | None | float) -> str:  # <- исправлено на str
     if value == '12':
-        return value == '12'
-    if value == 15:
-        return value == '15'
+        return "12"
+    elif value == 15:
+        return "15"
     if value == .5:
-        return value == '0.5'
+        return "0.5"
     if value is None:
-        return value == 'None'
-
+        return "None"
 
 if __name__ == "__main__":
     assert stringify(value="12") == "12"
