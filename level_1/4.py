5d73b3d0c54da3db09277620cc011658e937c6ff
RusssianNightmare <123409770+RusssianNightmare@users.noreply.github.com>
2026-04-11 15:25:11 +0300

    Update 4.py

--- level_1/4.py
+++ level_1/4.py
@@ -5,7 +5,6 @@ from constants import ___
 
 def calculate_age(date_of_birth: datetime.date) -> int:
     today = datetime.date(2022, 6, 2)
-    date_of_birth = datetime.date(1965, 6, 2)
     return today.year - date_of_birth.year
 
 
